35619e11cdcf421329c2d3ab472b7fe0d0dbc8b8
Hamza_Bouzidi <hamzabouzidi1999@gmail.com>
2023-03-29 05:18:05 +0200

    commit

---
+++ app/src/main/java/com/example/booky/ui/view/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.booky.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.booky.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
